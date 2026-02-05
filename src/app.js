78af205c128f9be37f4d88d555a4c55324dc9121
Claude-MC <dasilvaclaude30@gmail.com>
2026-02-05 07:17:14 +0100

    feat: installation et configuration de Swagger UI sur /api-docs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+// 1. Importation  des outils Swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./config/swagger');
 
 const app = express();
 
 app.use(express.json());
+
+// 2. route de la documentation 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.get("/", (req, res) => {
   res.json({ message: "Express JS fonctionne " });
