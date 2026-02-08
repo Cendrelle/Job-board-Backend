d75051f164fd3276e94f3d139839efbe918f209a
aoche20 <chegoun2019@gmail.com>
2026-02-08 17:28:47 +0100

    Gestion de candidature & fichiers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+const applyToJobController = require('../controllers/apply_to_jobController');
 // Inscription : POST /api/auth/register
 router.post('/register', authController.register);
 
 // Connexion : POST /api/auth/login
 router.post('/login', authController.login);
-
+// gestion des candidatures : POST /api/jobs/:id/apply
+router.post("/jobs/:id/apply", applyToJobController.applyToJob);
 module.exports = router;
